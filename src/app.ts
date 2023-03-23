7694d8c88141cbc7047f8ae7f4bdb94b79032936
Zackaliok <zackaliok.contact@gmail.com>
2023-03-23 16:09:24 +0100

    server fonctionnel

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/', (req: any, res: any) => {
+    res.status(201).send('Hello world !')
+});
+
+app.get('/users', (req: any, res: any) => {
+    res.status(200).send('Liste des utilisateurs')
+});
+
 app.listen(8080, () => {
     console.log("Server Up");
-})
+});
+
